2b8ca9cacd7c944fec02f603a92fb7b2a457e573
Matti Jokitulppo <melonmanchan@gmail.com>
2016-12-27 12:46:58 +0200

    Add validations for various errors that might happen when connecting

---
+++ src/socketio/events.js
@@ -0,0 +1,6 @@
+export default {
+  CONNECT       : 'connect',
+  CONNECT_ERROR : 'error',
+  NAME_TAKEN    : 'name_taken',
+  LOGGED_IN     : 'logged_in'
+}
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,8 +3,8 @@
     <form class="pure-form pure-form-stacked">
       <fieldset>
         <legend>Welcome to the chat</legend>
-        <input v-model="username" id="name" type="text" placeholder="Username" required>
-
+        <input v-bind:class="{error: errorMessage }" v-model="username" id="name" type="text" placeholder="Username" required>
+        <div v-if="errorMessage" class="error-message">{{errorMessage}}</div>
         <div class="pure-controls">
           <button :disabled="joinDisabled" v-on:click="join" type="submit" class="pure-button pure-button-primary">Join</button>
         </div>
@@ -15,13 +15,22 @@
 
 <script>
 import config from '../config/config'
-import events from '../socket/events'
+import events from '../socketio/events'
 
 export default {
   data () {
     return {
       username: '',
+      errorMessage: null,
       joinDisabled: false
+    }
+  },
+
+  watch: {
+    username (val) {
+      if (val === '') {
+        this.errorMessage = null
+      }
     }
   },
 
@@ -37,8 +46,20 @@ export default {
 
       window._socket = window.io(config.backend, { query: `username=${this.username}` })
 
-      window._socket.on(events.CONNECT, () => {
-        window.alert(1)
+      window._socket.once(events.LOGGED_IN, () => {
+        console.log('logged in')
+      })
+
+      window._socket.once(events.NAME_TAKEN, () => {
+        this.errorMessage = 'That username is taken!'
+        this.joinDisabled = false
+        window._socket.disconnect()
+      })
+
+      window._socket.once(events.CONNECT_ERROR, () => {
+        this.errorMessage = 'Something went wrong. Try again later.'
+        this.joinDisabled = false
+        window._socket.disconnect()
       })
 
       return false
@@ -52,7 +73,6 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
-
   height: 100vh;
 
   button {
@@ -63,5 +83,20 @@ export default {
   legend {
     text-align: center;
   }
+
+  .error {
+    background-color: #fce4e4;
+    border: 1px solid #cc0033;
+    outline: none;
+  }
+
+  .error-message {
+    color: #cc0033;
+    display: inline-block;
+    font-size: 12px;
+    line-height: 15px;
+    margin: 5px 0 0;
+  }
+
 }
 </style>
